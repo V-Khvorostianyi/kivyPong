63680c0b40085ea77475b4981da49b7174a2ad71
V-Khvorostianyi <slavafh@gmail.com>
2017-12-25 08:41:12 +0200

    create kivy pong app with apk in bin folder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
-from random import randint
+
 
 
 class PongPaddle(Widget):
